addb490899fa64785e9ff95684f3904560162e03
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-29 10:31:00 -0300

    Add routes user

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
 
+import { AuthUserController } from './controllers/AuthUserController'
+import { CreateUserController } from './controllers/CreateController'
+
+const createUserController = new CreateUserController()
+const authUserController = new AuthUserController()
+
 const routes = Router()
 
-routes.get('/')
+routes
+  .post('/new', createUserController.handle)
+  .get('/auth', authUserController.handle)
 
 export { routes }
